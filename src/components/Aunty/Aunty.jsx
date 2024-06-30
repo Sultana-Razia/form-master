35343d080d383deb12f1055a36961c5c39f6f44a
Sultana Razia <sultanarazia483@gmail.com>
2024-06-30 09:17:09 +0600

    How To Use Context API To Avoid Prop Drilling

--- src/components/Aunty/Aunty.jsx
+++ src/components/Aunty/Aunty.jsx
@@ -1,7 +1,10 @@
+import { useContext } from "react";
 import Cousin from "../Cousin/Cousin";
+import { MoneyContext } from "../Grandfather/Grandfather";
 
 
 const Aunty = () => {
+    const [money, setMoney] = useContext(MoneyContext);
     return (
         <div>
             <h2>Aunty</h2>
@@ -10,6 +13,8 @@ const Aunty = () => {
                 <Cousin name={'Naem'}></Cousin>
                 <Cousin name={'Mili'}></Cousin>
             </section>
+            <p>Money: {money}</p>
+            <button onClick={() => setMoney(money + 100)}>Add Money</button>
         </div>
     );
 };
--- src/components/Grandfather/Grandfather.jsx
+++ src/components/Grandfather/Grandfather.jsx
@@ -1,19 +1,29 @@
+import { createContext, useState } from "react";
 import Aunty from "../Aunty/Aunty";
 import Father from "../Father/Father";
 import Uncle from "../Uncle/Uncle";
 import './Grandfather.css';
 
+export const AssetContext = createContext('gold');
+export const MoneyContext = createContext(100);
 
 const Grandfather = () => {
     const asset = 'diamond';
+
+    const [money, setMoney] = useState(100);
     return (
         <div className="grandfather">
             <h2>Grandfather</h2>
-            <section className="flex">
-                <Father asset={asset}></Father>
-                <Uncle></Uncle>
-                <Aunty></Aunty>
-            </section>
+            <p>Money: {money}</p>
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <AssetContext.Provider value="gold">
+                    <section className="flex">
+                        <Father asset={asset}></Father>
+                        <Uncle></Uncle>
+                        <Aunty></Aunty>
+                    </section>
+                </AssetContext.Provider>
+            </MoneyContext.Provider>
         </div>
     );
 };
